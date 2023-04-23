294e9fd207703956a63d66816cd5f6d927bc4526
Genife <GenifeUA@artemzheglov10@gmail.com>
2023-04-23 15:48:08 +0300

    Config feature

--- src/main/java/com/genife/InfiniteSculk.java
+++ src/main/java/com/genife/InfiniteSculk.java
@@ -2,11 +2,14 @@ package com.genife;
 
 import com.genife.Commands.Command;
 import com.genife.Managers.SculkManager;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InfiniteSculk extends JavaPlugin {
     public static InfiniteSculk instance;
     private SculkManager sculkManager;
+    public static String START_MESSAGE;
+    public static String STOP_MESSAGE;
 
     public static InfiniteSculk getInstance() {
         return instance;
@@ -17,6 +20,17 @@ public final class InfiniteSculk extends JavaPlugin {
         instance = this;
 
         sculkManager = new SculkManager();
+
+        // Настраиваем конфиг
+        FileConfiguration config = getConfig();
+
+        config.addDefault("StartMessage", "§7[§eInfiniteSculk§7] §7Распространение успешно запущено!");
+        config.addDefault("StopMessage", "§7[§eInfiniteSculk§7] §7Все распространения завершены!");
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        START_MESSAGE = config.getString("StartMessage");
+        STOP_MESSAGE = config.getString("StopMessage");
 
         //noinspection DataFlowIssue
         this.getCommand("isculk").setExecutor(new Command());
